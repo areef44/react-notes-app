36b415f8d56e123f299feb2093171abfef148ced
areef44 <areef.empat.empat@gmail.com>
2025-01-19 12:39:47 +0700

    fixing some typescript error

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import "./styles/style.css";
 import App from "./App";
 
 // Non-null assertion untuk memastikan elemen root ada
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root")!);
 
 // Render elemen React ke dalam root
 root.render(
